84af9de8df5da1d34862e6ad5535c2efde394e61
Musawer Khan <musawerbangash109@gmail.com>
2024-04-26 20:41:02 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+"use strict";
+let a = 5;
+let b = 2;
+a++; //6
+b--; //1
+console.log(a);
+console.log(b);
+let c = 5;
+c++;
+console.log(c);
+let dd = 5;
+let bb = 2;
+let cc = ++dd + ++dd + --bb + --bb + dd + bb++ + bb;
+console.log(cc);
---
+++ main.ts
@@ -0,0 +1,20 @@
+// Increment and Discrement
+
+  let a:number = 5;
+  let b:number = 2
+  a++; //6
+  b--;//1
+  console.log(a)
+  console.log(b)
+
+  let c:number = 5;
+  c++;
+  console.log(c)
+
+  let dd:number = 5;
+  let bb:number = 2;
+  let cc:number = ++dd + ++dd + --bb + --bb + dd + bb++ +bb;
+  console.log(cc)
+
+
+ 
